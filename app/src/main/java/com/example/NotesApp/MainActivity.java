8c17e4b6597292c6033d70f2864962c09408476c
Madhuri B.H <madhuribh2001@gmail.com>
2020-10-02 17:01:12 +0530

    Added PagingAdapter

--- app/src/main/java/com/example/NotesApp/MainActivity.java
+++ app/src/main/java/com/example/NotesApp/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.NotesApp.List.NotesListPagingAdapter;
 import com.example.NotesApp.addData.AddViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewModel = new ViewModelProvider(this).get(AddViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        final NotesListPagingAdapter notesListPagingAdapter = new NotesListPagingAdapter();
+        recyclerView.setAdapter(notesListPagingAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         viewModel.GetAllNotes().observe(this, new Observer<PagedList<Notes>>() {
             @Override
             public void onChanged(PagedList<Notes> notes) {
-
+notesListPagingAdapter.submitList(notes);
             }
         });
     }
